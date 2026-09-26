166736fb3191ad3fd59a2686b98a8dbe8e956cd0
chandankumarn1 <2005chandankumarn@gmail.com>
2026-09-26 21:43:36 +0530

    Time: 3 ms (67.32%) | Memory: 15.9 MB (31.02%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void generate(string s, int open, int close, int n) {
+
+        if (s.length() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        // Add opening bracket
+        if (open < n) {
+            generate(s + "(", open + 1, close, n);
+        }
+
+        // Add closing bracket only when valid
+        if (close < open) {
+            generate(s + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        generate("", 0, 0, n);
+        return ans;
+    }
+};
